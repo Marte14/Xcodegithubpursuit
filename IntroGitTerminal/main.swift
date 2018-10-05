0a6bcf88d8719a7b1804901908756b6fde37dbc6
marte14 <kattymarte14@gmail.com>
2018-10-05 14:55:48 -0400

    initial commit

---
+++ IntroGitTerminal/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  IntroGitTerminal
+//
+//  Created by katty y marte on 10/5/18.
+//  Copyright © 2018 katty y marte. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
